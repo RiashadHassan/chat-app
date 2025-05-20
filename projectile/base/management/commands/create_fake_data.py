727d3c4eb8ea37fa43ff83d0baf2460eb46d487a
Riashad Hassan <riashadhassan1@gmail.com>
2025-05-20 22:43:15 +0600

    MGM: updated create_fake_data to trigger save()

--- projectile/base/management/commands/create_fake_data.py
+++ projectile/base/management/commands/create_fake_data.py
@@ -8,19 +8,23 @@ from django.contrib.auth.hashers import make_password
 from projectile.core.models import User
 from projectile.server.models import Server, Category, Channel, AuditLog, Thread
 
-fake = Faker()
-
 
 class Command(BaseCommand):
-    help = "fake data to make our lives easier"
+    help = "Fake data to make our lives easier"
+
+    def __init__(self):
+        super().__init__()
+        self.fake = Faker()
+        self.default_password = "123456"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--users", type=int, default=10, help="Number of users to create"
+        )
 
     def handle(self, *args, **options):
         self.create_superuser()
-
-        self.stdout.write("One SuperUser Created!!")
-        self.stdout.write("Now Creating Fake Data.......!!")
-
-        users = self.create_users()
+        users = self.create_users(count=options["users"])
         servers = self.create_servers(users)
         categories = self.create_categories(servers)
         channels = self.create_channels(categories)
@@ -35,113 +39,105 @@ class Command(BaseCommand):
                 first_name="Riashad",
                 last_name="Hassan",
                 email="riashad@gmail.com",
-                password="123456",
+                password=self.default_password,
                 phone="01827703876",
                 is_verified=True,
             )
+        self.stdout.write("One SuperUser Created!!")
 
-    def create_users(self):
+    def create_users(self, count):
         with transaction.atomic():
-            emails = list(set([fake.email() for _ in range(10)]))
+            emails = list(set([self.fake.email() for _ in range(count)]))
             try:
                 users_to_create = [
                     User(
                         email=email,
-                        first_name=fake.first_name(),
-                        last_name=fake.last_name(),
-                        username=fake.user_name(),
-                        phone=fake.phone_number(),
-                        password=make_password("123456"),
+                        first_name=self.fake.first_name(),
+                        last_name=self.fake.last_name(),
+                        username=self.fake.user_name(),
+                        phone=self.fake.phone_number(),
+                        password=make_password(self.default_password),
                     )
-                    for email in tqdm(emails)
+                    for email in tqdm(emails, desc="Creating Users...")
                 ]
                 User.objects.bulk_create(users_to_create, ignore_conflicts=True)
-                users = User.objects.filter(email__in=emails)
-
                 self.stdout.write("Fake Users created!")
+                users = User.objects.filter(email__in=emails)
                 return users
             except Exception as e:
                 self.stdout.write(str(e))
 
     def create_servers(self, users):
         with transaction.atomic():
-            try:
-                servers_to_create = [
-                    Server(
+            servers = []
+            for user in tqdm(users, desc="Creating Servers..."):
+                try:
+                    server = Server.objects.create(
                         owner=user,
-                        name=f"{user.username}'s-server-{fake.word()}",
-                        description=fake.paragraph(),
+                        name=f"{user.username}'s-server-{self.fake.word()}",
+                        description=self.fake.paragraph(),
                     )
-                    for user in tqdm(users)
-                ]
-                Server.objects.bulk_create(servers_to_create, ignore_conflicts=True)
-                servers = Server.objects.filter(owner__in=users)
-                self.stdout.write("Fake Servers created!")
-                return servers
-            except Exception as e:
-                self.stdout.write(str(e))
+                    servers.append(server)
+                except:
+                    self.stdout.write(f"Error creating Server for {user.email}!")
+                    continue
+            self.stdout.write("Fake Servers created!")
+            return servers
 
     def create_categories(self, servers):
         with transaction.atomic():
-            try:
-                categories_to_create = [
-                    Category(
-                        name=f"Category: {fake.word()}",
+            categories = []
+            for index, server in tqdm(
+                enumerate(servers), desc="Creating Categories..."
+            ):
+                try:
+                    category = Category.objects.create(
+                        name=f"Category: {self.fake.word()}",
                         server=server,
                         position=float(index),
                     )
-                    for index, server in tqdm(enumerate(servers))
-                ]
-                Category.objects.bulk_create(
-                    categories_to_create, ignore_conflicts=True
-                )
-                categories = Category.objects.filter(server__in=servers).select_related(
-                    "server"
-                )
-                self.stdout.write("Fake Categories created!")
-                return categories
-            except Exception as e:
-                self.stdout.write(str(e))
+                    categories.append(category)
+                except:
+                    self.stdout.write(f"Error creating Category for {server.uid}!")
+                    continue
+            self.stdout.write("Fake Categories created!")
+            return categories
 
     def create_channels(self, categories):
         with transaction.atomic():
-            try:
-                channels_to_create = [
-                    Channel(
-                        name=f"Channel: {fake.word()}",
+            channels = []
+            for index, category in tqdm(
+                enumerate(categories), desc="Creating Channels..."
+            ):
+                try:
+                    channel = Channel.objects.create(
+                        name=f"Channel: {self.fake.word()}",
                         category=category,
                         server=category.server,
                         position=float(index),
                     )
-                    for index, category in tqdm(enumerate(categories))
-                ]
-                Channel.objects.bulk_create(channels_to_create, ignore_conflicts=True)
-
-                channels = Channel.objects.filter(
-                    category__in=categories
-                ).select_related("server", "category")
-
-                self.stdout.write("Fake Channels created!")
-
-                return channels
-
-            except Exception as e:
-                self.stdout.write(str(e))
+                    channels.append(channel)
+                except:
+                    self.stdout.write(f"Error creating Channel for {category.uid}!")
+                    continue
+            self.stdout.write("Fake Channels created!")
+            return channels
 
     def create_threads(self, channels):
         with transaction.atomic():
-            try:
-                threads_to_create = [
-                    Thread(
-                        name=f"Thread: {fake.word()}",
+            threads = []
+            for index, channel in tqdm(enumerate(channels), desc="Creating Threads..."):
+                try:
+                    thread = Thread.objects.create(
+                        name=f"Thread: {self.fake.word()}",
                         channel=channel,
                         category=channel.category,
                         server=channel.category.server,
+                        position=float(index),
                     )
-                    for channel in tqdm(channels)
-                ]
-                Thread.objects.bulk_create(threads_to_create, ignore_conflicts=True)
-                self.stdout.write("Fake Threads created!")
-
-            except Exception as e:
-                self.stdout.write(str(e))
+                    threads.append(thread)
+                except:
+                    self.stdout.write(f"Error creating Thread for {channel.uid}!")
+                    continue
+            self.stdout.write("Fake Threads created!")
+            return threads
