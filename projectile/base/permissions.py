694bd860a61790aa448b81d649e785d485c7efef
Riashad Hassan <riashadhassan1@gmail.com>
2025-06-04 17:49:42 +0600

    feat: new OrPermission class created

--- projectile/base/permissions.py
+++ projectile/base/permissions.py
@@ -4,7 +4,27 @@ from rest_framework.permissions import BasePermission
 class IsSuperUser(BasePermission):
     def has_permission(self, request, view):
         return (
-            request.user
-            and request.user.is_authenticated
-            and request.user.is_superuser
+            request.user and request.user.is_authenticated and request.user.is_superuser
         )
+
+
+class OrPermission:
+    def __init__(self, *permission_classes):
+        self.permission_classes = permission_classes
+
+    def __call__(self):
+        permission_classes = self.permission_classes
+
+        class _OrPermission(BasePermission):
+            def has_permission(self, request, view):
+                return any(
+                    perm().has_permission(request, view) for perm in permission_classes
+                )
+
+            def has_object_permission(self, request, view, obj):
+                return any(
+                    perm().has_object_permission(request, view, obj)
+                    for perm in permission_classes
+                )
+
+        return _OrPermission()
