f6dc62046d79c9d2fb482345680076f37dfe6410
Riashad Hassan <riashadhasa@gmail.com>
2025-10-21 14:55:34 +0600

    feat: frontend app initial setup

---
+++ frontend/src/pages/login/page.jsx
@@ -0,0 +1,44 @@
+const LoginBackdrop = () => {
+    return (
+        <div className="login-bg-image">
+        </div>
+    )
+}
+const LoginPortal = () => {
+    return (
+        <div className="login-portal-container">
+            <h1>Login Portal</h1>
+        </div>
+    )
+}
+const LoginPage = () => {
+    return (
+        <div>
+            <LoginBackdrop />
+            <LoginPortal /></div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default LoginPage
+
