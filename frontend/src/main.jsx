5874a48deeada475f89bddc6a2d1da826c47cbd1
Riashad Hassan <riashadhassan1@gmail.com>
2025-12-03 21:30:41 +0600

    frontend(feat): added Routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import LoginPage from './pages/login/page'
-import "./styles/login.css"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import LoginPage from "./pages/login/page";
+import "./styles/login.css";
 
-createRoot(document.getElementById('root')).render(
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <LoginPage />
-  </StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
